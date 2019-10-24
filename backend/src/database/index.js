1c4a3094ed744aec4b5c2cfce8ae27f404f52b24
Daniel Oliveira <oliveiradaniel@protonmail.com>
2019-10-24 00:33:41 -0300

    chore(backend): add models loader

---
+++ backend/src/database/index.js
@@ -0,0 +1,22 @@
+import Sequelize from 'sequelize';
+
+import User from '../app/models/User';
+import Student from '../app/models/Student';
+
+import databaseConfig from '../config/database';
+
+const models = [User, Student];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+
+    models.forEach(model => model.init(this.connection));
+  }
+}
+
+export default new Database();
