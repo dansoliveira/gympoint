1b3e86aefd2f91cddb7ca68ba083bf4cf0b234cb
Daniel Oliveira <oliveiradaniel@protonmail.com>
2019-10-24 02:12:47 -0300

    feat(backend): add Student manipulation
    List all students. Register and update an student.

---
+++ backend/src/app/controllers/StudentController.js
@@ -0,0 +1,51 @@
+import Student from '../models/Student';
+
+class StudentController {
+  async index(req, res) {
+    const students = await Student.findAll();
+
+    return res.json(students);
+  }
+
+  async store(req, res) {
+    const studentExists = await Student.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (studentExists) {
+      return res.status(400).json({ error: 'Student already exists.' });
+    }
+
+    const student = await Student.create(req.body);
+
+    return res.json(student);
+  }
+
+  async update(req, res) {
+    const { email } = req.body;
+    const student = await Student.findByPk(req.params.id);
+
+    if (email !== student.email) {
+      const studentExists = await Student.findOne({
+        where: { email },
+      });
+
+      if (studentExists) {
+        return res.status(400).json({ error: 'Student already exists.' });
+      }
+    }
+
+    const { id, name, age, weight, height } = await student.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      age,
+      weight,
+      height,
+    });
+  }
+}
+
+export default new StudentController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
+import StudentController from './app/controllers/StudentController';
 
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
+
+routes.get('/students', StudentController.index);
+routes.post('/students', StudentController.store);
+routes.put('/students/:id', StudentController.update);
 
 export default routes;
