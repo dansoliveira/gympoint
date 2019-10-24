7530e925cb841bf28140f7b7b5d678280417034e
Daniel Oliveira <oliveiradaniel@protonmail.com>
2019-10-24 01:21:36 -0300

    feat(backend): add JWT authentication to User

--- backend/src/app/models/User.js
+++ backend/src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -12,6 +13,12 @@ class User extends Model {
         sequelize,
       }
     );
+
+    return this;
+  }
+
+  comparePassword(password) {
+    return bcrypt.compare(password, this.password_hash);
   }
 }
 
